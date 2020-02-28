a699e4bdedcded68af26b0347b7e35d50c02d1e3
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-02-28 08:37:59 -0600

    file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Animal Style');
+INSERT INTO burgers (name) VALUES ('Plain & Dry');
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name) VALUES ('Double Double');
